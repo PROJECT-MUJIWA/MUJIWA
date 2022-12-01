43f9ccb047707932930d911f332741ea51df31f9
Adalbertus250 <killergames156@gmail.com>
2022-12-01 20:51:27 +0100

    Příprava cest pro funkci invite registrace

--- routes/auth.php
+++ routes/auth.php
@@ -5,4 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::controller(\App\Http\Controllers\Auth\RegisteredUserController::class)->group(function(){
     Route::get('/register', 'index')->name('register');
     Route::post('/register', 'store');
+    Route::get('/register/invite', 'indexInvite')->name('register.invite');
+    Route::post('/register/invite', 'storeInvite');
 })->middleware(['guest']);
