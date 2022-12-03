72d240c02663828951e983191ba1adab49b3584a
Adalbertus250 <killergames156@gmail.com>
2022-12-03 20:53:41 +0100

    Odstraněna kontrola existence globální hodnoty _translations
    Dělalo to teď bordel, bude pak doplněno

--- resources/client/utils/i18n.js
+++ resources/client/utils/i18n.js
@@ -1,7 +1,5 @@
-export default function useI18n(key, replacement = {})
+export default function (key, replacement = {})
 {
-    if(!'_translations' in window || window._translations !== null) throw Error('Funkci nelze použít, není definován _translations v globálním window');
-
     let translation = window._translations[key] || key;
 
     Object.keys(replacement).forEach(r => {
