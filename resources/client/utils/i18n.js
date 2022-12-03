fc65206e4e1bff641a4e5577cc5c5525b9e065d2
Adalbertus250 <killergames156@gmail.com>
2022-12-03 21:01:57 +0100

    Fix replace funkce

--- resources/client/utils/i18n.js
+++ resources/client/utils/i18n.js
@@ -3,7 +3,7 @@ export default function (key, replacement = {})
     let translation = window._translations[key] || key;
 
     Object.keys(replacement).forEach(r => {
-        translation = translation.replace(`${r}`, replacement[r]);
+        translation = translation.replace(`:${r}`, replacement[r]);
     });
 
     return translation;
