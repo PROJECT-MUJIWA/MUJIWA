f212e62ca3ab13cf0f6456e4ccf13e475cdaa981
Adalbertus250 <killergames156@gmail.com>
2022-12-03 20:34:18 +0100

    Přidána logika pro transformaci PHP & JSON i18n
    Tato komponenta bude insert překlady do global window na stránce, čímž umožní inertii a reactu tyto překlady využívat v aplikaci

--- app/View/Components/Translation.php
+++ app/View/Components/Translation.php
@@ -2,6 +2,9 @@
 
 namespace App\View\Components;
 
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\File;
 use Illuminate\View\Component;
 
 class Translation extends Component
@@ -17,12 +20,63 @@ class Translation extends Component
     }
 
     /**
+     * Map PHP Language Function
+     *
+     * @param $lang string
+     * @return array
+     */
+    protected function mapPHPLanguage(string $lang): array
+    {
+        return collect(File::allFiles(lang_path($lang)))
+            ->filter(function ($file){
+                return $file->getExtension() === "php";
+            })->flatMap(function ($file){
+                return Arr::dot(File::getRequire($file->getRealPath()));
+            })->toArray();
+    }
+
+    /**
+     * Map JSON Language Function
+     *
+     * @param $lang string
+     * @return array
+     */
+    protected function mapJSONLanguage(string $lang): array
+    {
+        return json_decode(File::get(lang_path("$lang.json")), true);
+    }
+
+    /**
      * Get the view / contents that represent the component.
      *
      * @return \Illuminate\Contracts\View\View|\Closure|string
      */
     public function render()
     {
-        return view('components.translation');
+        $locale = App::getLocale();
+
+        $phpTranslation = [];
+        $jsonTranslation = [];
+
+        if(File::exists(lang_path($locale)))
+        {
+            $phpTranslation = $this->mapPHPLanguage($locale);
+        }
+
+        if(!File::exists(lang_path($locale)))
+        {
+            $phpTranslation = $this->mapPHPLanguage('en');
+        }
+
+        if(File::exists(lang_path("$locale.json")))
+        {
+            $jsonTranslation = $this->mapJSONLanguage($locale);
+        }
+
+        $translations = array_merge($phpTranslation, $jsonTranslation);
+
+        return view('components.translation', [
+            'translations' => $translations
+        ]);
     }
 }
