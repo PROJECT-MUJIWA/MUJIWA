d65f81765aacc1c85851d49947bf1e3e454cac26
Adalbertus250 <adalbertus@mujiwa.cz>
2025-02-26 09:35:29 +0100

    chore: nastavení CSP

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,23 +3,27 @@
 declare(strict_types=1);
 
 use App\Http\Middleware\HandleInertiaRequests;
+use App\Http\Middleware\InjectCspNonceMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Spatie\Csp\AddCspHeaders;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
-        health: '/up',
+        health: '/health',
     )
     ->withMiddleware(function (Middleware $middleware): void {
 
         /**
-         * Nastavení InertiaJS middlewaru
+         * Nastavení middlewares
          */
         $middleware->web(append: [
+            InjectCspNonceMiddleware::class,
             HandleInertiaRequests::class,
+            AddCspHeaders::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
