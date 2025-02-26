deb0d2066de3d9c0e3471ade8387c126ab8fc3c1
Adalbertus250 <adalbertus@mujiwa.cz>
2025-02-26 10:36:32 +0100

    chore: Nastavení App a type coverage

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,23 +4,52 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
+use Carbon\CarbonImmutable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Date;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 
 final class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
-    public function register(): void
-    {
-        //
-    }
-
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        //
+        $this->configureCommands();
+        $this->configureDates();
+        $this->configureModels();
+        $this->configureVite();
+        $this->configureUrl();
+    }
+
+    private function configureCommands(): void
+    {
+        DB::prohibitDestructiveCommands(
+            $this->app->isProduction()
+        );
+    }
+
+    private function configureDates(): void
+    {
+        Date::use(CarbonImmutable::class);
+    }
+
+    private function configureModels(): void
+    {
+        Model::shouldBeStrict(! $this->app->isProduction());
+        Model::unguard();
+    }
+
+    private function configureVite(): void
+    {
+        Vite::useAggressivePrefetching();
+    }
+
+    private function configureUrl(): void
+    {
+        if ($this->app->isProduction()) {
+            URL::forceHttps();
+        }
     }
 }
